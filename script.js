6c52ded827e0c8a1b435bb03ca50251001c3df44
diogo <diogo@EERIEGOESD>
2023-09-12 02:33:53 +0100

    Initial commit for my website

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById('clickMe');
+    button.addEventListener('click', function() {
+        alert('Hello, world!');
+    });
+});
